501ff806aa25f63e5bab96d40a3ac34a5035cc4b
rlejr135 <rlejr135@gmail.com>
2019-04-07 00:49:28 +0900

    HW1 ing.....

--- program_mode/clock.c
+++ program_mode/clock.c
@@ -1,17 +1,18 @@
 #include "../20141602.h"
 
 
-#define CHANGE_SET 0;
-#define RESET      1;
-#define HOUR_INC   2;
-#define MIN_INC    3;
+#define CHANGE_SET 0
+#define RESET      1
+#define HOUR_INC   2
+#define MIN_INC    3
 
 struct mode1_state{
-	int hour, min;
+	unsigned int hour, min;
+	unsigned int reset_hour, reset_min;
 	unsigned char led[8];
 
 
-}
+};
 
 struct mode1_state *clock_state ;
 
@@ -30,22 +31,42 @@ void mode1_construct(){
 	clock_state = (struct mode1_state*)malloc(sizeof(struct mode1_state));
 	timer = time(0);
 	t = localtime(&timer);
-	clock_state->hour = t->tm_hour;
-	clock_state->min = t->tm_min;
+	clock_state->reset_hour = clock_state->hour = t->tm_hour;
+	clock_state->reset_min = clock_state->min = t->tm_min;
+
+	printf("%d %d\n",clock_state->hour, clock_state->min);
+
 }
 
 
-int mode1_main(int swinum){
-		static unsigned char sw1_flag = 0;
+struct mo_msgbuf mode1_main(unsigned char *swinum){
+	static unsigned int sw1_flag = 0;
+	int i= 0, switch_number = -1;
+	while(i < 4){
+		if (swinum[i] == 1)
+			switch_number = i;
+		i++;
+	}
 
-		switch (swinum){
-			case CHANGE_SET:
-							break;
-			case RESET:
-							break;
-			case HOUR_INC:
-							break;
-			case MIN_INC:
-							break;
-		}
+	switch (switch_number){
+		case CHANGE_SET:
+			sw1_flag  = (sw1_flag + 1) & 1;
+			break;
+		case RESET:
+			clock_state->hour = clock_state->reset_hour;
+			clock_state->min = clock_state->reset_min;
+			break;
+		case HOUR_INC:
+			if (sw1_flag == 0) break;
+			clock_state->hour += 1;
+			clock_state->hour = clock_state->hour % 24;
+			break;
+		case MIN_INC:
+			if (sw1_flag == 0) break;
+			clock_state->min += 1;
+			clock_state->min =clock_state->min % 60;
+			break;
+	}
+	printf("flag : %d number : %d\n", sw1_flag, switch_number);
+	printf("%d %d\n", clock_state->hour, clock_state->min);
 }
